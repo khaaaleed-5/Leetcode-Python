0f26fe6bd7d8ebacbd7e8993c663281a4bf3a199
khaaaleed-5 <khaled.20210298@gmail.com>
2024-01-17 19:18:58 +0200

    sss

--- Medium/347_Top K Frequent Elements.py
+++ Medium/347_Top K Frequent Elements.py
@@ -5,6 +5,6 @@ class Solution:
         for i in nums:
             frqL[i] = frqL.get(i,0) + 1
         frqL = dict(sorted(frqL.items(),key=lambda item: item[1], reverse=True))
-        for key in list(frqL.keys())[:k]:
+        for key in (frqL.keys())[:k]:
             ans.append(key)
         return ans
