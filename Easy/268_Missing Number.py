f4d07bc6f754eead47a0641d9d70ec3b1bd20c4e
khaaaleed-5 <khaled.20210298@gmail.com>
2024-02-20 18:41:18 +0200

    268. Missing Number

--- Easy/268_Missing Number.py
+++ Easy/268_Missing Number.py
@@ -1,8 +1,18 @@
 class Solution:
     def missingNumber(self, nums: list[int]) -> int:
-        nums.sort()
+        n = len(nums)
+        return (n * (n + 1) // 2) - sum(nums)
+    
+    """
+    
+    another soution : 
+    
+    nums.sort()
         n = len(nums)
         for i in range(n):
             if i != nums[i]:
                 return i
         return n
+        
+    """
+        
