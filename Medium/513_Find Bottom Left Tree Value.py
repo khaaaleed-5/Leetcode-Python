f3605e38cbf52c56c63818e976454999273338e1
khaaaleed-5 <khaled.20210298@gmail.com>
2024-02-28 14:39:44 +0200

    513. Find Bottom Left Tree Value

---
+++ Medium/513_Find Bottom Left Tree Value.py
@@ -0,0 +1,22 @@
+from collections import deque
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def findBottomLeftValue(self, root: TreeNode) -> int:
+        queue = deque([root])
+        leftmost_value = None
+
+        while queue:
+            node = queue.popleft()
+
+            leftmost_value = node.val
+
+            if node.right:
+                queue.append(node.right)
+            if node.left:
+                queue.append(node.left)
+
+        return leftmost_value
