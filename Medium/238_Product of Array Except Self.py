3be6f8b77ed4e4bbe69c2776cbb28173ffc5b98c
khaaaleed-5 <khaled.20210298@gmail.com>
2024-03-15 13:17:28 +0200

    238. Product of Array Except Self

---
+++ Medium/238_Product of Array Except Self.py
@@ -0,0 +1,19 @@
+class Solution:
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        value = 1
+        ans = []
+        cnt_0 = nums.count(0)
+        if cnt_0 > 1:
+            return [0] * len(nums)
+        for i in nums:
+            if i != 0:
+                value *= i
+        for i in nums:
+            if cnt_0:
+                if i == 0:
+                    ans += [value]
+                else:
+                    ans += [0]
+            else:
+                ans += [value // i]
+        return ans  
