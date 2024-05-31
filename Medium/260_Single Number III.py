42a11ca458b529452ff5fae77a10c0edab81a53f
Khaled Ashraf <105022395+khaaaleed-5@users.noreply.github.com>
2024-05-31 15:59:08 +0300

    Create 260_Single Number III.py

---
+++ Medium/260_Single Number III.py
@@ -0,0 +1,8 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        mp = Counter(nums)
+        ans = []
+        for key,val in mp.items():
+            if val == 1:
+                ans.append(key)
+        return ans
