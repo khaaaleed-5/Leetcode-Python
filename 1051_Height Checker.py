68c8c95dd680d0e996b6b5b2f8bfd17ba9bebf42
Khaled Ashraf <105022395+khaaaleed-5@users.noreply.github.com>
2024-06-10 11:10:43 +0300

    Delete 1051_Height Checker.py

--- 1051_Height Checker.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def heightChecker(self, heights: List[int]) -> int:
-        heights1 = sorted(heights)
-        ans = 0
-        for i in range(len(heights)):
-            if heights[i] != heights1[i]:
-                ans += 1
-        return ans
