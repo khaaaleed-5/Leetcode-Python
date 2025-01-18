637cdea2711d39b139dd7864fe21601b67777143
Khaled Ashraf <105022395+khaaaleed-5@users.noreply.github.com>
2025-01-18 15:28:00 +0200

    Update 347_Top K Frequent Elements.py

--- Medium/347_Top K Frequent Elements.py
+++ Medium/347_Top K Frequent Elements.py
@@ -1,10 +1,20 @@
 class Solution:
     def topKFrequent(self, nums: list[int], k: int) -> list[int]:
-        frqL = {}
-        ans = []
-        for i in nums:
-            frqL[i] = frqL.get(i,0) + 1
-        frqL = dict(sorted(frqL.items(),key=lambda item: item[1], reverse=True))
-        for key in list(frqL.keys())[:k]:
-            ans.append(key)
-        return ans
+        mp = Counter(nums)
+        output = []
+        for key in mp.most_common(k):
+            output += [key[0]]
+        return output
+        
+        
+        
+        
+# ------------Another-Solution------------------------------        
+        # frqL = {}
+        # ans = []
+        # for i in nums:
+        #     frqL[i] = frqL.get(i,0) + 1
+        # frqL = dict(sorted(frqL.items(),key=lambda item: item[1], reverse=True))
+        # for key in list(frqL.keys())[:k]:
+        #     ans.append(key)
+        # return ans
