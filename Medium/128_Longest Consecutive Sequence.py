c89aa45b3abdd65ce019fe3901177ad110c9e36d
Khaled Ashraf <105022395+khaaaleed-5@users.noreply.github.com>
2025-01-20 20:54:31 +0200

    Update 128_Longest Consecutive Sequence.py
    O(n) solution

--- Medium/128_Longest Consecutive Sequence.py
+++ Medium/128_Longest Consecutive Sequence.py
@@ -1,16 +1,28 @@
 class Solution:
     def longestConsecutive(self, nums: list[int]) -> int:
-        nums = sorted(set(nums))
-        cnt = 1
-        ans = [1]
-        for i in range(len(nums) - 1):
-            if (nums[i] + 1 == nums[i + 1]):
-                cnt += 1
-            else:
-                ans.append(cnt)
-                cnt = 1
-            ans.append(cnt)
-        if not nums:
-            return 0
-        else:
-            return max(ans)
+        nums = set(nums)
+        ans = 0
+        for num in nums:
+            if num - 1 not in nums:
+                idx = 1
+                while num + idx in nums:
+                    idx += 1
+                ans = max(ans,idx)
+        return ans
+
+
+# -----------------Another-Solution----------------------- O(nlogn)       
+#         nums = sorted(set(nums))
+#         cnt = 1
+#         ans = [1]
+#         for i in range(len(nums) - 1):
+#             if (nums[i] + 1 == nums[i + 1]):
+#                 cnt += 1
+#             else:
+#                 ans.append(cnt)
+#                 cnt = 1
+#             ans.append(cnt)
+#         if not nums:
+#             return 0
+#         else:
+#             return max(ans)
